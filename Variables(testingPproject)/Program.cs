82cab61beff3248f6ab5ddc684113a885042bb8b
Alexander_Rykov <reboot1899@gmail.com>
2023-01-16 15:31:32 -0600

    Work with arrays

--- Variables(testingPproject)/Program.cs
+++ Variables(testingPproject)/Program.cs
@@ -2,7 +2,7 @@
 
 
 
-namespace ForIteration
+namespace AnderstandingArrays
 {
     class Program
     {
@@ -156,7 +156,7 @@ namespace ForIteration
 
 
             // Iteration
-            for (int i = 0; i < 19; i++)
+            /*for (int i = 0; i < 19; i++)
             {
                 //Console.WriteLine(i);
                 if (i == 7)
@@ -165,15 +165,82 @@ namespace ForIteration
                     break;
                 }
             }
+
+            for (int myValue = 0; myValue < 12; myValue++)
+            {
+                Console.WriteLine(myValue);
+            }
+
+
             Console.ReadLine();
+            */
+
+            // Work with arrays
+            /*
+            int number1 = 4;
+            int number2 = 8;
+            int number3 = 15;
+            int number4 = 16;
+            int number5 = 23;
+
+            if (number1 == 16)
+            {
+
+            }
+            else if (number2 == 16)
+            {
+
+            }
+            else if (number3 == 16)
+            {
+
+            }
+            */
+            /*
+            int[] numbers = new int[5];
+
+            numbers[0] = 4;
+            numbers[1] = 8;
+            numbers[2] = 15;
+            numbers[3] = 16;
+            numbers[4] = 23;
+
+            // Console.WriteLine(numbers[4]);
+            Console.WriteLine(numbers.Length);
+            Console.ReadLine();
+            */
 
 
+            //int[] numbers = new int[] { 4, 8, 15, 16, 23, 42 };
 
+            string[] names = new string[] { "Eddie", "Alex", "Michael" };
+            /*
+            for (int i = 0; i < names.Length; i++)
+            {
+                Console.WriteLine(names[i]);
+            }
+            Console.ReadLine();
+            */
 
+            /*
+            foreach (string name in names)
+            {
+                Console.WriteLine(name);
+            }
+            Console.ReadLine();
+            */
 
+            string zig = "You can get what you want out of life if you help" +
+                " enough other people get what they want.";
 
+            char[] charArray = zig.ToCharArray();
+            Array.Reverse(charArray);
 
-
+            foreach (char zigChar in charArray)
+            {
+                Console.Write(zigChar);
+            }
+            Console.ReadLine();
 
 
 
